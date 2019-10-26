dbb5c9cd4ccd5ed49eddf377236fabe3ceb0316c
Mac <mac@MacBook-Pro-Mac.local>
2019-10-26 12:47:01 +0300

    Updates

---
+++ precache-manifest.309389a79d156f10370e4bc7d73b1783.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15a4de1c67364638d4939f5c97a2a64c",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "1ea4cfb442b0ca08d469",
+    "url": "/Monsters-Rolodex/static/css/main.f9b1f621.chunk.css"
+  },
+  {
+    "revision": "122726f0f318abb699c9",
+    "url": "/Monsters-Rolodex/static/js/2.a56bac0d.chunk.js"
+  },
+  {
+    "revision": "1ea4cfb442b0ca08d469",
+    "url": "/Monsters-Rolodex/static/js/main.98e45c68.chunk.js"
+  },
+  {
+    "revision": "92c7adb392711be590bd",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.688950c7.js"
+  }
+]);
